28b0c2a425b2a8dbd27e001020c168d241da5d2d
Yuri Kunde Schlesner <yuriks@yuriks.net>
2017-06-10 18:26:21 -0700

    Have build script take boost path as a parameter instead of env var

--- build.cmd
+++ build.cmd
@@ -4,9 +4,9 @@ bcp ^
     boost/container/static_vector.hpp ^
     boost/crc.hpp ^
     boost/icl/interval_map.hpp ^
+    boost/optional.hpp ^
     boost/range/algorithm/ ^
     boost/range/algorithm_ext/ ^
     boost/smart_ptr/intrusive_ptr.hpp ^
-    boost/optional.hpp ^
     boost/variant/ ^
-    --boost="%BOOST_PATH%" .
+    --boost="%1" .
