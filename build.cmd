3abc84abaf63a068cb59a9f9b5675c1947bc6fd9
Yuri Kunde Schlesner <yuriks@yuriks.net>
2017-06-10 18:37:13 -0700

    Add boost::container::small_vector

--- build.cmd
+++ build.cmd
@@ -1,6 +1,7 @@
 bcp ^
     boost/container/flat_map.hpp ^
     boost/container/flat_set.hpp ^
+    boost/container/small_vector.hpp ^
     boost/container/static_vector.hpp ^
     boost/crc.hpp ^
     boost/icl/interval_map.hpp ^
