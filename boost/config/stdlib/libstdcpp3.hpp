43274a0449937ec36b1afc32378a60b48869c805
fearlessTobi <thm.frey@gmail.com>
2019-08-24 15:39:04 +0200

    Update boost to 1.71 and add asio

--- boost/config/stdlib/libstdcpp3.hpp
+++ boost/config/stdlib/libstdcpp3.hpp
@@ -299,6 +299,9 @@ extern "C" char *gets (char *__s);
 #if (BOOST_LIBSTDCXX_VERSION < 70100) || (__cplusplus <= 201402L)
 #  define BOOST_NO_CXX17_STD_INVOKE
 #  define BOOST_NO_CXX17_STD_APPLY
+#  define BOOST_NO_CXX17_HDR_OPTIONAL
+#  define BOOST_NO_CXX17_HDR_STRING_VIEW
+#  define BOOST_NO_CXX17_HDR_VARIANT
 #endif
 
 #if defined(__has_include)
--- build.cmd
+++ build.cmd
@@ -1,14 +1,18 @@
 bcp ^
+    boost/asio.hpp ^
     boost/container/flat_map.hpp ^
     boost/container/flat_set.hpp ^
     boost/container/small_vector.hpp ^
     boost/container/static_vector.hpp ^
     boost/crc.hpp ^
+    boost/date_time/posix_time/posix_time.hpp ^
     boost/functional/hash.hpp ^
     boost/icl/interval_map.hpp ^
     boost/optional.hpp ^
     boost/range/algorithm/ ^
     boost/range/algorithm_ext/ ^
     boost/smart_ptr/intrusive_ptr.hpp ^
+    boost/system/error_code.hpp ^
+    boost/system/system_error.hpp ^
     boost/variant/ ^
     --boost="%1" .
